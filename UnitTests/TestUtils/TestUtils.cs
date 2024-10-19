345024173374cd4e202222bc5393fded91733fb6
No.1 N[][]b <91905916+no-1-noob@users.noreply.github.com>
2024-10-19 16:19:11 +0200

    TestUtilsFix

--- UnitTests/TestUtils/TestUtils.cs
+++ UnitTests/TestUtils/TestUtils.cs
@@ -10,7 +10,7 @@ namespace UnitTests.TestUtils
     {
         public static BeatmapLevel CreateBeatmapLevel()
         {
-            return new BeatmapLevel(false, string.Empty, string.Empty, string.Empty, string.Empty, new string[0], new string[0], 1, 1, 1, 1, 1, 1, PlayerSensitivityFlag.Explicit, null, null);
+            return new BeatmapLevel(1,false, string.Empty, string.Empty, string.Empty, string.Empty, new string[0], new string[0], 1, 1, 1, 1, 1, 1, PlayerSensitivityFlag.Explicit, null, null);
         }
 
         public static BeatmapKey CreateBeatmapKey()
